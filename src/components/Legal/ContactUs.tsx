47d6e095568ba6af49e423ad0ecafc386bcd765f
kanchandaskuldeep1954-ctrl <kanchandaskuldeep1954@gmail.com>
2026-01-18 18:24:58 +0530

    docs: update contact information with user details

--- src/components/Legal/ContactUs.tsx
+++ src/components/Legal/ContactUs.tsx
@@ -25,8 +25,8 @@ const ContactUs: React.FC = () => {
                     <div className="space-y-8">
                         {[
                             { title: 'Email Us', info: 'support@toeasy.ai', icon: 'M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z' },
-                            { title: 'Call Center', info: '+1 (555) 123-4567', icon: 'M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z' },
-                            { title: 'Global HQ', info: '123 Governance Way, San Francisco, CA', icon: 'M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z M15 11a3 3 0 11-6 0 3 3 0 016 0z' }
+                            { title: 'Call Center', info: '+91 7509348544', icon: 'M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z' },
+                            { title: 'Global HQ', info: 'Madan Mahal, Jabalpur, Madhya Pradesh 482001', icon: 'M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z M15 11a3 3 0 11-6 0 3 3 0 016 0z' }
                         ].map((item, idx) => (
                             <div key={idx} className="flex gap-6 items-start group">
                                 <div className="w-12 h-12 rounded-2xl bg-indigo-500/10 flex items-center justify-center text-indigo-600 group-hover:scale-110 transition-transform">
